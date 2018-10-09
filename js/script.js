6ea691b3dd70c12b09a93263194ab5d34ba3248a
MammaSonnim <msonnim@gmail.com>
2018-10-10 00:48:05 +0300

    add tallinn slided

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-var scalableItems = document.querySelectorAll('.js-scalable');
 
-window.addEventListener('scroll', function(evt) {
-    scalableItems.forEach(function(item) {
-        item.classList.add('js-scalable_fix');
-    })
-});
