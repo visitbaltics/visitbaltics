91327576d642118db2a59d9a0e7c5e530986603a
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2018-10-13 22:02:33 +0300

    upd loading

--- js/script.js
+++ js/script.js
@@ -2,8 +2,36 @@
 
     var offset = Math.floor(window.innerHeight / 4);
 
-    new WOW({
-        offset: offset
-    }).init();
+    var preloadElt = document.querySelector('.preload');
 
+    if (preloadElt) {
+        const promiseTo
+        preloadImages()
+            .then(() => {
+                    preloadElt.classList.remove('hidden');
+
+                    new WOW({
+                        offset: offset
+                    }).init();
+            });
+    }
 })();
+
+function preloadImages() {
+    var images = document.querySelectorAll('img');
+    var promises = Array.prototype.map.call(images, function (image) {
+        return new Promise(function (resolve) {
+            image.onload = function () {
+                resolve();
+            };
+
+            image.onerror = function name() {
+                resolve();
+            };
+
+            image.src = image.src;
+        })
+    });
+
+    return Promise.all(promises);
+}
